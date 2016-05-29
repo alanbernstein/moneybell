402e08680bf1d4262d3f787f2929481555dc943f
Alan Bernstein <abernstein@umbel.com>
2016-05-29 00:08:12 -0500

    Clean and rename stuff

--- arduino/neopixel_ring/neopixel_ring.ino
+++ arduino/neopixel_ring/neopixel_ring.ino
@@ -37,15 +37,15 @@ uint32_t WW = strip.Color(255, 255, 255, 255);
 uint32_t buffer[NUM_PIXELS];
 
 /*
-struct cycle {
+struct spinner {
   int length;      // in pixels
   uint32_t color;  // base color
   int delay;       // update time
   bool direction;  // 0=CW, 1=CCW
 };
 
-cycle cycle1 = {10, R, 10, 0};
-cycle cycle2 = {5, G, 5, 1};
+spinner spinner1 = {10, R, 10, 0};
+spinner spinner2 = {5, G, 5, 1};
 */
 
 void setup() {
@@ -55,11 +55,11 @@ void setup() {
   Serial.begin(9600);
   delay(500);
   Serial.println("start");
-  //cycle_init(10, 0, R);
+  //spinner_init(10, 0, R);
   strip.show();
 }
 
-void cycle_init(int len, int offset, uint32_t color) {
+void spinner_init(int len, int offset, uint32_t color) {
   for(uint16_t i=offset; i<len; i++) {
     strip.setPixelColor(i % NUM_PIXELS, color);
   }
@@ -73,73 +73,57 @@ int num_updates;
 
 void loop() {
   //ring_test();
-  
+
+  // animate_arbitrary_time_space();
   /*  
-  for(uint16_t i=0; i<60; i++) {
-    color = get_ring_color(t, i);
-    strip.setPixelColor(i, color);
-    delay(5);
-  }
   */
   /*
-  update_cycle(t, 10, 1, R);
+  update_spinner(t, 10, 1, R);
   delay(20);
   */
   
   time = millis();
-  num_updates = update_cycles(time);
-  //Serial.println(String(time) + " " + String(num_updates));
+  num_updates = update_spinners(time);
   if(num_updates > 0) {
-    draw_cycles_to_buffer();
+    draw_spinners_to_buffer();
     strip.show();
   }
   t++;
 }
 
 // how do i struct
-int pixel;
-const int NUM_CYCLES = 3;
-int starts[NUM_CYCLES] = {0, 30, 0};
-int lengths[NUM_CYCLES] = {8, 10, 12};
-int dirs[NUM_CYCLES] = {1, -1, 1};
-uint32_t colors[NUM_CYCLES] = {R, G, B};
-int delays[NUM_CYCLES] = {25, 90, 50};
-int last_updates[NUM_CYCLES] = {0, 0, 0};
+const int NUM_SPINNERS = 3;
+int starts[NUM_SPINNERS] = {0, 30, 0};
+int lengths[NUM_SPINNERS] = {8, 10, 12};
+int dirs[NUM_SPINNERS] = {1, -1, 1};
+uint32_t colors[NUM_SPINNERS] = {R, G, B};
+int delays[NUM_SPINNERS] = {25, 90, 50};
+int last_updates[NUM_SPINNERS] = {0, 0, 0};
 
-int num_to_use = NUM_CYCLES;
-
-int update_cycles(unsigned long time) {
-  //Serial.println("<start update_cycles>");
+int update_spinners(unsigned long time) {
   int num_updates = 0;
-  for(int cycle_idx=0; cycle_idx<num_to_use; cycle_idx++) {
-    if(time - last_updates[cycle_idx] > delays[cycle_idx]) {
-      starts[cycle_idx] = mod(starts[cycle_idx] + dirs[cycle_idx], NUM_PIXELS);
-      last_updates[cycle_idx] = time;
+  for(int spinner_idx=0; spinner_idx<NUM_SPINNERS; spinner_idx++) {
+    if(time - last_updates[spinner_idx] > delays[spinner_idx]) {
+      starts[spinner_idx] = mod(starts[spinner_idx] + dirs[spinner_idx], NUM_PIXELS);
+      last_updates[spinner_idx] = time;
       num_updates++;
-      // Serial.println("update " + String(cycle_idx) + " - " + String(starts[cycle_idx]));
     }
   }
-  //Serial.println("<end update_cycles>");
   return num_updates;
 }
 
 
-void draw_cycles_to_buffer() {
-  //Serial.println("<start draw_cycles_to_buffer>");
+void draw_spinners_to_buffer() {
+  int pixel;
   clear_buffer();
-  for(int cycle_idx=0; cycle_idx<num_to_use; cycle_idx++) {
-    //Serial.println("drawing " + String(starts[cycle_idx]) + ", " + String(lengths[cycle_idx]));
-    for(int n=0; n<lengths[cycle_idx]; n++) {
-      pixel = mod(starts[cycle_idx] + dirs[cycle_idx] * n, NUM_PIXELS);
+  for(int spinner_idx=0; spinner_idx<NUM_SPINNERS; spinner_idx++) {
+    for(int n=0; n<lengths[spinner_idx]; n++) {
+      pixel = mod(starts[spinner_idx] + dirs[spinner_idx] * n, NUM_PIXELS);
       color = strip.getPixelColor(pixel);
-      new_color = color | decay_color(colors[cycle_idx], lengths[cycle_idx] - 1 - n);
-      //new_color = color | colors[cycle_idx];
+      new_color = color | decay_color(colors[spinner_idx], lengths[spinner_idx] - 1 - n);
       strip.setPixelColor(pixel, new_color);
-      //Serial.println("draw " + String(pixel) + " " + String(new_color & 255) + " " + String(new_color & 65280));
-      // decay here for gradient tail
     }
   }
-  //Serial.println("<end draw_cycles_to_buffer>");
 }
 
 uint32_t decay_color(uint32_t color, int n) {
@@ -151,9 +135,6 @@ uint32_t decay_color(uint32_t color, int n) {
   green = decay_kernel_exponential(green, n);
   blue = decay_kernel_exponential(blue, n);
   white = decay_kernel_exponential(white, n);
-  //Serial.println(String(blue) + " " + String(red) + " " + String(green) + " " + String(white) + " " + String(n));
-  //Serial.println(color);
-  //Serial.println(red << 8 | blue);
   return white << 24 | green << 16 | red << 8 | blue; 
 }
 
@@ -175,13 +156,21 @@ void clear_buffer() {
 /////////////////////////////////////////
 
 
-void update_cycle(int t, int len, int speed, uint32_t color) {
+void update_spinner(int t, int len, int speed, uint32_t color) {
   int head_index = (speed*t+len) % NUM_PIXELS;
   int tail_index = speed*t % NUM_PIXELS;
   strip.setPixelColor(tail_index, K);
   strip.setPixelColor(head_index, color);
 }
 
+
+void animate_arbitrary_time_space() {
+  for(uint16_t i=0; i<60; i++) {
+    color = get_ring_color(t, i);
+    strip.setPixelColor(i, color);
+    delay(5);
+  }
+}
 
 uint32_t c[10] = {R, G, B, C, M, Y, W, WR, WG, WB};
 uint32_t get_ring_color(int t, int i) {
