340c5c62c4dab32959a5f0f9b9b98cc4e268571e
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2017-03-08 19:59:38 -0600

    update comments

--- arduino/strip_test/strip_test.ino
+++ arduino/strip_test/strip_test.ino
@@ -1,14 +1,5 @@
-// Simple NeoPixel test.  Lights just a few pixels at a time so a
-// 1m strip can safely be powered from Arduino 5V pin.  Arduino
-// may nonetheless hiccup when LEDs are first connected and not
-// accept code.  So upload code first, unplug USB, connect pixels
-// to GND FIRST, then +5V and digital pin 6, then re-plug USB.
-// A working strip will show a few pixels moving down the line,
-// cycling between red, green and blue.  If you get no response,
-// might be connected to wrong end of strip (the end wires, if
-// any, are no indication -- look instead for the data direction
-// arrows printed on the strip).
-
+// connect lights to pins 0,1,2,3,4
+// connect trigger signal to pin 10
 #include <Adafruit_NeoPixel.h>
 
 #define PIN      0
